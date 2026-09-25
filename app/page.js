aa5353455792393fa178a593a12604468fd0a909
rusudaniyaflanishvili-max <rusudaniyaflanishvili@gmail.com>
2026-09-25 04:48:55 +0400

    Add Home component with catalog information

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>წონის ბალანსის ფორმულა</h1>
+      <p>2026 Premium Catalog</p>
+      <p>ყავა • ჩაი • კაფსულები • ნაკრებები</p>
+
+      <a href="https://wa.me/995577049828">
+        შეკვეთა WhatsApp-ზე
+      </a>
+    </main>
+  );
+}
